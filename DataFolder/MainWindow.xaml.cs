92f2ca05079b34ed3b465fcac4699d36ff174e5a
trophi20 <trophi20@users.noreply.github.com>
2020-05-10 23:24:43 +0200

    Erste Anpassungen Oberfläche

--- DataFolder/MainWindow.xaml.cs
+++ DataFolder/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,9 +21,42 @@ namespace DataFolder
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		//enum Dateieigenschaft
+
+		string filepathSource;
+		string filepathDestination;
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void ButtonFolderSource_Click(object sender, RoutedEventArgs e)
+		{
+			FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+			folderDlg.SelectedPath = filepathSource;
+			DialogResult result = folderDlg.ShowDialog();
+			if (result == System.Windows.Forms.DialogResult.OK)
+			{
+				filepathSource = folderDlg.SelectedPath;
+				InputFolderSource.Text = filepathSource;
+			}
+		}
+
+		private void ButtonFolderDestination_Click(object sender, RoutedEventArgs e)
+		{
+			FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+			DialogResult result = folderDlg.ShowDialog();
+			if (result == System.Windows.Forms.DialogResult.OK)
+			{
+				filepathDestination = folderDlg.SelectedPath;
+				InputFolderDestination.Text = filepathDestination;
+			}
+		}
+
+		private void ButtonStart_Click(object sender, RoutedEventArgs e)
+		{
+			List<string> files = new List<string>();
+
 		}
 	}
 }
