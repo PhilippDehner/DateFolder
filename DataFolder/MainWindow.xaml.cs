82b74e9e14cb0639fabc62919f00a787f55eccd6
trophi20 <trophi20@users.noreply.github.com>
2020-05-23 19:24:01 +0200

    Funktionen hinzugefügt: Dateien sortieren nach Änderungsdatum oder Erstelldatum Trennzeichen

--- DataFolder/MainWindow.xaml.cs
+++ DataFolder/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Forms;
 
@@ -18,7 +20,9 @@ namespace DataFolder
 		{
 			InitializeComponent();
 			StartPossible();
+			TrennzeichenGeaendert();
 			CopyMove_Move.IsChecked = true;
+			DatumGeaendert.IsChecked = true;
 		}
 
 		private void ButtonFolderSource_Click(object sender, RoutedEventArgs e)
@@ -48,19 +52,29 @@ namespace DataFolder
 
 		private void ButtonStart_Click(object sender, RoutedEventArgs e)
 		{
+			ButtonStart.IsEnabled = false;
+			Fortschritt.Value = 0;
+
 			string[] files = new string[0];
 			if (CheckSubFolder.IsChecked ?? false)
 				files = Directory.GetFiles(filepathSource, "*", SearchOption.AllDirectories);
 			else
 				files = Directory.GetFiles(filepathSource, "*", SearchOption.TopDirectoryOnly);
+			Fortschritt.Maximum = files.Length;
 
-			foreach(string file in files)
+			foreach (string file in files)
 			{
-				DateTime dt = File.GetLastWriteTime(file);
+				DateTime dt = new DateTime();
+				if(DatumGeaendert.IsChecked ?? false)
+					dt = File.GetLastWriteTime(file);
+				if (DatumCreated.IsChecked ?? false)
+					dt = File.GetCreationTime(file);
+
 				string year = dt.Year.ToString();
 				string month = dt.ToString("MM", System.Globalization.DateTimeFormatInfo.InvariantInfo);
 				string day = dt.ToString("dd", System.Globalization.DateTimeFormatInfo.InvariantInfo);
-				string newFolder = filepathDestination + "\\" + year + "\\" + year + "-" + month + "\\" + year + "-" + month + "-" + day;
+				string tz = InputTrennzeichen.Text;
+				string newFolder = filepathDestination + "\\" + year + "\\" + year + tz + month + "\\" + year + tz + month + tz + day;
 				Directory.CreateDirectory(newFolder);
 				try
 				{
@@ -73,12 +87,29 @@ namespace DataFolder
 				{
 					System.Windows.Forms.MessageBox.Show(ex.ToString());
 				}
+				Fortschritt.Value++;
 			}
+			Process.Start(filepathDestination);
+			ButtonStart.IsEnabled = true;
 		}
 
 		private void StartPossible()
 		{
 			ButtonStart.IsEnabled = filepathDestination != "" && filepathDestination != null && filepathSource != "" && filepathSource != null;
+		}
+
+		private void InputTrennzeichen_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+		{
+			if (App.Current.MainWindow.IsInitialized)
+			{
+				TrennzeichenGeaendert();
+			}
+		}
+
+		void TrennzeichenGeaendert()
+		{
+			string tz = InputTrennzeichen.Text;
+			Variante1.Content = "Variante 1: 3 Ebenen (Stufe 1: JJJJ | Stufe 2: JJJJ" + tz + "MM | Stufe 3: JJJJ" + tz + "MM" + tz + "TT)";
 		}
 	}
 }
